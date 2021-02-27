e172cc38b18c9868fd90d3ad6d7aaec25efed294
a_karnaukhova <karn-av@protonmail.com>
2021-02-28 00:30:54 +0300

    Добавляет смену жанра и фильтрацию списка фильмов по жанру

---
+++ src/components/genre-tabs/genre-tabs.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import {createGenreTypesList} from '../../utils/utils'
+import FilmsList from '../films-list/films-list';
+import {connect} from 'react-redux';
+import {ActionCreator} from '../../store/action';
+import {filterFilmsByGenre} from '../../utils/utils';
+import {GenreType} from '../../utils/const'
+
+const GenreTabs = (props) => {
+  const {films, genre, onTabClick} = props;
+
+  const filterTypesList = Array.from(createGenreTypesList(films));
+  filterTypesList.unshift(GenreType.ALL);
+
+  const currentGenre = filterTypesList.find((element) => element === genre);
+
+  const renderFilteredFilmsList = (films, genre) => {
+    switch (genre) {
+      case GenreType.ALL:
+        return (
+          <FilmsList
+            films={films}
+          />
+        );
+      case currentGenre:
+        let filteredFilms = filterFilmsByGenre(films, currentGenre);
+
+        return (
+          <FilmsList
+            films={filteredFilms}
+          />
+        );
+      default:
+        return (
+          <FilmsList
+            films={films}
+          />
+        );
+    }
+  };
+
+  const renderGenreTypesList = () => {
+    const genreTypesList = [];
+    
+    filterTypesList.forEach((item) => {
+      genreTypesList.push(
+      <li className={`catalog__genres-item ${genre === item ? `catalog__genres-item--active` : ``}`} onClick={onTabClick} key={item}>
+        <Link to="#" className="catalog__genres-link">{item}</Link>
+      </li>)
+    });
+
+    return genreTypesList;
+  }
+
+  return (
+    <React.Fragment>
+      <ul className="catalog__genres-list">
+        {renderGenreTypesList()}
+      </ul>
+
+      {renderFilteredFilmsList(films, currentGenre)}
+    </React.Fragment>
+  );
+};
+
+GenreTabs.propTypes = {
+  films: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  genre: state.genre,
+  films: state.films,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onTabClick(evt) {
+    dispatch(ActionCreator.changeGenre(evt));
+  }
+});
+
+export {GenreTabs};
+export default connect(mapStateToProps, mapDispatchToProps)(GenreTabs);
--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../header/header';
-import FilmsList from '../films-list/films-list.jsx';
+import GenreTabs from '../genre-tabs/genre-tabs';
 import Footer from '../footer/footer';
 
 const MainPage = (props) => {
-  const {promoName, promoGenre, promoReleaseDate, films} = props;
+  const {promoName, promoGenre, promoReleaseDate} = props;
 
   return (
     <div>
@@ -54,42 +54,7 @@ const MainPage = (props) => {
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-          <ul className="catalog__genres-list">
-            <li className="catalog__genres-item catalog__genres-item--active">
-              <a href="#" className="catalog__genres-link">All genres</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Comedies</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Crime</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Documentary</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Dramas</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Horror</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Kids & Family</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Romance</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Sci-Fi</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Thrillers</a>
-            </li>
-          </ul>
-
-          <FilmsList
-            films={films}
-          />
+          <GenreTabs />
 
           <div className="catalog__more">
             <button className="catalog__button" type="button">Show more</button>
