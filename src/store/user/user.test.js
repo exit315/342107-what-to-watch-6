6f0a57dc5cce7ad8758ea310a7059b17ae736b6f
a_karnaukhova <akarnauhova@gallery-chizhov.com>
2021-04-07 17:59:20 +0300

    Добавляет тест для авторизации

--- src/store/user/user.test.js
+++ src/store/user/user.test.js
@@ -1,5 +1,11 @@
 import {user} from './user';
 import {ActionType} from '../action';
+import MockAdapter from 'axios-mock-adapter';
+import {createAPI} from '../../api/api';
+import {checkAuth, login} from '../../api/api-actions';
+import {AppRoute} from '../../utils/const';
+
+const api = createAPI(() => {});
 
 describe(`Reducers work correctly`, () => {
   it(`Reducer without additional parameters should return initial state`, () => {
@@ -32,5 +38,65 @@ describe(`Reducers work correctly`, () => {
 
     expect(user(state, requireAuthAction))
       .toEqual({authorizationStatus: true, userEmail: null});
+  });
+});
+
+describe(`Async operation work correctly`, () => {
+  const responseEmail = `test@test.ru`;
+
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthLoader = checkAuth();
+
+    apiMock
+      .onGet(AppRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REMEMBER_USER,
+          payload: responseEmail,
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REQUIRE_AUTHORIZATION,
+          payload: true,
+        });
+      });
+  });
+
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeUser = {email: `test@test.ru`, password: `123456`};
+    const loginLoader = login(fakeUser);
+
+    apiMock
+      .onPost(AppRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return loginLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(3);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.DISABLE_FORM,
+          payload: false,
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REMEMBER_USER,
+          payload: fakeUser.email,
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: ActionType.REQUIRE_AUTHORIZATION,
+          payload: true,
+        });
+      });
   });
 });
